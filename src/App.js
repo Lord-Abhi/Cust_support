d494d71966a7ecd5bf74c95050daa423c9c09680
Abhinash Dora <dora.abhinash67@gmail.com>
2025-01-22 19:43:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 //import VoiceTest from './VoiceTest.tsx';
 import ChatBot from './ChatBot.js'
+import './css/Header.css'
+import Logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <div className="logo">Customer Support Simulation</div>
+        <div className="user-info">
+          { <img src={Logo} alt="Decorative background" className="header-right-image" /> }
+        </div>
+      </div>
       {/* <VoiceTest /> */}
       <ChatBot />
     </div>
